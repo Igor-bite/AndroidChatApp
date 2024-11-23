fa091adfee957e406928d3c0aca3e180418c6135
Igor Klyuzhev <klyuzhevigor@gmail.com>
2024-11-24 00:33:43 +0300

    feat: Basic chats list screen

--- app/src/main/java/com/klyuzhevigor/ChatApp/ChatsList/ChatsListScreen.kt
+++ app/src/main/java/com/klyuzhevigor/ChatApp/ChatsList/ChatsListScreen.kt
@@ -1,12 +1,41 @@
 package com.klyuzhevigor.ChatApp.ChatsList
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.klyuzhevigor.ChatApp.Auth.AuthManager
 
 @Composable
 fun ChatsListScreen() {
-    Text("ChatsListScreen")
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top) {
+        Text("Chats", fontSize = 44.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(Modifier.height(20.dp))
+
+        LazyColumn() {
+            items(listOf("1", "2")) { el ->
+                ChatCell(name = el)
+            }
+        }
+    }
+}
+
+@Composable
+fun ChatCell(name: String) {
+    Text("Hello " + name, fontSize = 28.sp, modifier = Modifier.fillMaxWidth())
 }
 
 class ChatsListViewModel(val authManager: AuthManager) {
--- app/src/main/java/com/klyuzhevigor/ChatApp/MainActivity.kt
+++ app/src/main/java/com/klyuzhevigor/ChatApp/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChatAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize().padding(vertical = 64.dp)) { innerPadding ->
                     val navController = rememberNavController()
                     val auth = AuthManager()
                     NavHost(navController, startDestination = "login") {
